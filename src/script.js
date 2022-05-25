a697eb32a5221dc6a78b2af9c763aba1e8e76b11
Cate Carpenter <ccarpenter5@ewu.edu>
2022-05-24 20:17:49 -0700

    update files

--- src/script.js
+++
@@ -1,7 +0,0 @@
-import CircleType from "https://cdn.skypack.dev/circletype@2.3.0";
-
-// Instantiate `CircleType` with an HTML element.
-const circleType = new CircleType(document.getElementById("title"));
-
-// Set the text radius and direction. Note: setter methods are chainable.
-circleType.radius(700).dir(1);
